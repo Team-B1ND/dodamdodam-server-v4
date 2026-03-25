9e75eb48e6f29893aacf57e4d50641191da4a1c7
kmg <legolove08@naver.com>
2026-03-25 09:27:07 +0900

    fix: gateway 심자, 외박 문제 해결

--- services/service-out-sleeping/src/main/resources/database/migration/V2__rename_reject_to_deny.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE out_sleepings CHANGE COLUMN reject_reason deny_reason VARCHAR(255);
-
-UPDATE out_sleepings SET status = 'DENIED' WHERE status = 'REJECTED';
--- services/service-out-sleeping/src/main/resources/database/migration/V6__insert_default_deadline.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO out_sleeping_deadlines (start_day_of_week, start_time, end_day_of_week, end_time, created_at, modified_at)
-VALUES ('SUNDAY', '00:00:00', 'SUNDAY', '17:00:00', NOW(), NOW());
