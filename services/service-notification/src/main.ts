ccdb6a3b8fb62c52ce93529af0b89cd80a137f32
kmg <legolove08@naver.com>
2026-03-25 17:17:00 +0900

    test: test deploy

--- services/service-notification/src/main.ts
+++ services/service-notification/src/main.ts
@@ -5,7 +5,6 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { GlobalExceptionFilter } from './common/filter/global-exception.filter';
 
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
