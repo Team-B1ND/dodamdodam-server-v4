50fe27d0c9318506d688aa6ce43db43961cedddc
ParkJunSeok <jdksla0129@gmail.com>
2026-05-14 21:43:13 +0900

    feat:/nightstudy/ban에 학생정보 추가

--- services/service-nightstudy/src/main/kotlin/com/b1nd/dodamdodam/nightstudy/application/ban/data/BanMapper.kt
+++ services/service-nightstudy/src/main/kotlin/com/b1nd/dodamdodam/nightstudy/application/ban/data/BanMapper.kt
@@ -1,7 +1,9 @@
 package com.b1nd.dodamdodam.nightstudy.application.ban.data
 
+import com.b1nd.dodamdodam.grpc.user.UserResponse
 import com.b1nd.dodamdodam.nightstudy.application.ban.data.request.BanUserRequest
 import com.b1nd.dodamdodam.nightstudy.application.ban.data.response.BannedUserResponse
+import com.b1nd.dodamdodam.nightstudy.application.ban.data.response.DormitoryManagerBannedUserResponse
 import com.b1nd.dodamdodam.nightstudy.domain.nightstudy.entity.NightStudyBannedEntity
 
 fun BanUserRequest.toEntity() = NightStudyBannedEntity(
@@ -18,3 +20,25 @@ fun NightStudyBannedEntity.toResponse() = BannedUserResponse(
 )
 
 fun List<NightStudyBannedEntity>.toResponseList() = map { it.toResponse() }
+
+fun List<NightStudyBannedEntity>.toDormitoryManagerResponseList(
+    usersMap: Map<String, UserResponse>,
+): List<DormitoryManagerBannedUserResponse> = mapNotNull { ban ->
+    usersMap[ban.userId.toString()]?.let { user ->
+        DormitoryManagerBannedUserResponse(
+            userId = ban.userId,
+            reason = ban.reason,
+            name = user.name,
+            student = user.student?.let {
+                DormitoryManagerBannedUserResponse.StudentInfo(
+                    grade = it.grade,
+                    room = it.room,
+                    number = it.number,
+                )
+            } ?: error("could not find user"),
+            phone = user.phone.orEmpty(),
+            endAt = ban.endAt,
+            createdAt = ban.createdAt!!,
+            )
+    }
+}
