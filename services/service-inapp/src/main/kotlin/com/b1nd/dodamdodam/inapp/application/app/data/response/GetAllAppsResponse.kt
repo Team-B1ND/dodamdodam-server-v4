4f5463dacfa748966dd7e567eb3b98b455eb5253
ParkJunSeok <jdksla0129@gmail.com>
2026-08-12 16:16:56 +0900

    feat: 앱 아이콘 추가 반환

--- services/service-inapp/src/main/kotlin/com/b1nd/dodamdodam/inapp/application/app/data/response/GetAllAppsResponse.kt
+++ services/service-inapp/src/main/kotlin/com/b1nd/dodamdodam/inapp/application/app/data/response/GetAllAppsResponse.kt
@@ -6,14 +6,16 @@ import java.util.UUID
 data class GetAllAppsResponse(
     val appId: UUID?,
     val name: String,
-    val subtitle: String
+    val subtitle: String,
+    val iconUrl: String
 ) {
     companion object {
         fun of(app: AppEntity) =
             GetAllAppsResponse(
                 appId = app.publicId,
                 name = app.name,
-                subtitle = app.subtitle
+                subtitle = app.subtitle,
+                iconUrl = app.iconUrl
             )
 
         fun fromList(apps: List<AppEntity>): List<GetAllAppsResponse> {
