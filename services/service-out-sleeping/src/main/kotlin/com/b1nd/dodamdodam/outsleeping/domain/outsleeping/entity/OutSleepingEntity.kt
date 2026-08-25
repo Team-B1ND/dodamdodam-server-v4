25bbbc446e46867ccb966914d3c5ecef27f0cba1
ParkJunSeok <jdksla0129@gmail.com>
2026-08-25 14:54:39 +0900

    feat: 마이그레이션 추가

--- services/service-out-sleeping/src/main/kotlin/com/b1nd/dodamdodam/outsleeping/domain/outsleeping/entity/OutSleepingEntity.kt
+++ services/service-out-sleeping/src/main/kotlin/com/b1nd/dodamdodam/outsleeping/domain/outsleeping/entity/OutSleepingEntity.kt
@@ -43,6 +43,7 @@ class OutSleepingEntity(
     var denyReason: String? = null,
 
     @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 20)
     var statusType: OutSleepingStatusType,
 ) : BaseTimeEntity() {
 
